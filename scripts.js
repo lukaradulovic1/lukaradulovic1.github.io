e704a4b66a92ca5a84fede5e0c771abf1e0f58f4
lukaradulovicdocusketch <luka.radulovic.011@gmail.com>
2024-05-10 12:22:07 +0200

    Added functions to hide mail and phone untill u click on them

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("phone").addEventListener("click", function (e) {
+  document.getElementById("phone").innerHTML = "+381 64 48 27 488";
+});
+
+document.getElementById("email").addEventListener("click", function (e) {
+  document.getElementById("email").innerHTML = "luka.radulovic.011@gmail.com";
+});
